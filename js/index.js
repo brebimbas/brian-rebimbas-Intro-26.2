5459f5f37c2e3e752cdbe43e625f7f5e292aa5ff
Brian Rebimbas <brebimbas@gmail.com>
2026-06-10 00:06:37 -0400

    Fix repo link

--- js/index.js
+++ js/index.js
@@ -85,7 +85,7 @@ document.addEventListener("DOMContentLoaded", () => {
     projectSection.appendChild(projectList);
   }
 
-  fetch("https://api.github.com/users/brebimbas/reposgit")
+  fetch("https://api.github.com/users/brebimbas/repos")
     .then((response) => {
       if (!response.ok) throw new Error("Request failed");
       return response.json();
